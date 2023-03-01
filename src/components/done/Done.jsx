c600947e9c1c52c12ec1808f545941facfab3066
Seungwoo Park <qpfgud430@gmail.com>
2023-03-01 16:08:24 +0900

    refactor: Done 분리

---
+++ src/components/done/Done.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  CancelButton,
+  CompleteButton,
+  DeleteButton,
+  H1,
+  Todo,
+  Wrapper,
+  Wrappers,
+} from "../todoList/style";
+
+function Done({ todos, deleteBtn, cancelBtn }) {
+  return (
+    <>
+      <H1>Done..🎉</H1>
+      <Wrapper>
+        {todos
+          .filter((item) => item.done === true)
+          .map((item) => {
+            return (
+              <Todo key={item.id}>
+                <h2>{item.title}</h2>
+                <h4>{item.content}</h4>
+                <DeleteButton onClick={() => deleteBtn(item.id)}>
+                  삭제
+                </DeleteButton>
+                <CancelButton onClick={() => cancelBtn(item.id)}>
+                  취소
+                </CancelButton>
+              </Todo>
+            );
+          })}
+      </Wrapper>
+    </>
+  );
+}
+
+export default Done;
