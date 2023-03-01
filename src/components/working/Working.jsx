caaf180155971e8ab5e6bfe8fc7d1b916d034e18
Seungwoo Park <qpfgud430@gmail.com>
2023-03-01 16:08:00 +0900

    refactor: working 분리

---
+++ src/components/working/Working.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  CancelButton,
+  CompleteButton,
+  DeleteButton,
+  H1,
+  Todo,
+  Wrapper,
+  Wrappers,
+} from "../todoList/style";
+
+function Working({todos, deleteBtn, completeBtn}) {
+  return (
+    <>
+      <H1>Working.. 🔥</H1>
+      <Wrapper>
+        {todos
+          .filter((item) => item.done !== true)
+          .map((item) => {
+            return (
+              <Todo key={item.id}>
+                <h2>{item.title}</h2>
+                <h4>{item.content}</h4>
+                <DeleteButton onClick={() => deleteBtn(item.id)}>
+                  삭제
+                </DeleteButton>
+                <CompleteButton onClick={() => completeBtn(item.id)}>
+                  완료
+                </CompleteButton>
+              </Todo>
+            );
+          })}
+      </Wrapper>
+    </>
+  );
+}
+
+export default Working;
